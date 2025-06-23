7bd72ddf2a6f08c52e43c7cd7db0bc0b75af7c3e
MrFil27 <78660195+MrFil27@users.noreply.github.com>
2025-06-23 21:19:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,15 @@
-
-
 module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv:///shiva:shiva@musicbot.ouljywv.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityType: "LISTENING",
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
