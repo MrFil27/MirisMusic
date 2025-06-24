c7767932eb800dab97db47776e9dee8173619e93
MrFil27 <78660195+MrFil27@users.noreply.github.com>
2025-06-24 11:06:26 +0200

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -21,7 +21,7 @@ async function connectToDatabase() {
     try {
         await client.connect();
         console.log('\n' + '─'.repeat(40));
-        console.log($`{colors.magenta}${colors.bright}🕸️  DATABASE CONNECTION${colors.reset}`);
+        console.log(`${colors.magenta}${colors.bright}🕸️  DATABASE CONNECTION${colors.reset}`);
         console.log('─'.repeat(40));
         console.log('\x1b[36m[ DATABASE ]\x1b[0m', '\x1b[32mConnected to MongoDB ✅\x1b[0m');
     } catch (err) {
