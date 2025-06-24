1a5a40450a8aaf0e7b71b97fda582a3f6c85a43d
MrFil27 <78660195+MrFil27@users.noreply.github.com>
2025-06-24 09:57:46 +0200

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,41 +1,63 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const colors = require('./UI/colors/colors');
 const config = require("./config.js");
 require('dotenv').config();
 
-let client; 
+let client;
 
-if (config.mongodbUri) {
+if(config.mongodbUri){
     const uri = config.mongodbUri;
-    client = new MongoClient(uri);
-} else {
+    client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        },
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    });
+}else{
     console.warn("\x1b[33m[ WARNING ]\x1b[0m MongoDB URI is not defined in the configuration.");
 }
 
-async function connectToDatabase() {
-    if (!client) {
+async function connectToDatabase(){
+    if(!client){
         console.warn("\x1b[33m[ WARNING ]\x1b[0m Skipping MongoDB connection as URI is not provided.");
         return;
     }
 
-    try {
+    try{
         await client.connect();
+        await client.db("admin").command({ ping: 1 });
         console.log('\n' + '─'.repeat(40));
         console.log(`${colors.magenta}${colors.bright}🕸️  DATABASE CONNECTION${colors.reset}`);
         console.log('─'.repeat(40));
         console.log('\x1b[36m[ DATABASE ]\x1b[0m', '\x1b[32mConnected to MongoDB ✅\x1b[0m');
-    } catch (err) {
+    }catch(err){
         console.warn("\x1b[33m[ WARNING ]\x1b[0m Could not connect to MongoDB. Continuing without database functionality.");
-        console.error(err.message); // Optional: log detailed error message
+        console.error(err.message);
+        return;
     }
 }
 
-const db = client ? client.db("PrimeMusicSSRR") : null;
-const playlistCollection = db ? db.collection("SongPlayLists") : null;
-const autoplayCollection = db ? db.collection("AutoplaySettings") : null;
+let db;
+let playlistCollection;
+let autoplayCollection;
+
+async function initializeCollections(){
+    if(client && client.isConnected()){
+        db = client.db("PrimeMusicSSRR");
+        playlistCollection = db.collection("SongPlayLists");
+        autoplayCollection = db.collection("AutoplaySettings");
+    }else{
+        console.warn("\x1b[33m[ WARNING ]\x1b[0m MongoDB connection not established. Collections will be unavailable.");
+    }
+}
+
+connectToDatabase().then(() => initializeCollections());
 
 module.exports = {
     connectToDatabase,
-    playlistCollection,
-    autoplayCollection,
+    getPlaylistCollection: () => playlistCollection,
+    getAutoplayCollection: () => autoplayCollection,
 };
